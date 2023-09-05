ee67c57a2646eab64734f585703da12533cb40c8
abidhasanpintu <abidhasanpintu@gmail.com>
2023-09-05 15:23:18 +0600

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,7 +1,8 @@
 (function($) {
 	$(document).ready(function(){
-		$.getJSON('./data/terms.json', function(data) {
+		$.getJSON('/wp-content/themes/area-theme-live/assets/vue/js/data/terms.json', function(data) {
             window.termsData = data;
+			$('body').append('<script src="/wp-content/themes/area-theme-live/assets/vue/js/area-research.chunk-vendors.js"></script><script src="/wp-content/themes/area-theme-live/assets/vue/js/area-research.app.js"></script>');
 		});
 	});
 }(jQuery));
