a524d7116f523f66d3c2d2562fd32de371dba015
abidhasanpintu <abidhasanpintu@gmail.com>
2023-09-05 16:32:48 +0600

    custom

--- custom.js
+++ custom.js
@@ -1,7 +1,16 @@
 (function($) {
 	$(document).ready(function(){
+		$.getJSON('/wp-content/themes/area-theme-live/assets/vue/js/data/topics.json', function(data) {
+            window.topicsData = data;
+		});
 		$.getJSON('/wp-content/themes/area-theme-live/assets/vue/js/data/terms.json', function(data) {
             window.termsData = data;
+		});
+		$.getJSON('/wp-content/themes/area-theme-live/assets/vue/js/data/category.json', function(data) {
+            window.categoryData = data;
+		});
+		$.getJSON('/wp-content/themes/area-theme-live/assets/vue/js/data/data.json', function(data) {
+            window.mainData = data;
 			setTimeout(function(){
 				$('body').append('<script src="/wp-content/themes/area-theme-live/assets/vue/js/area-research.app.js"></script>');
 			}, 1000);
