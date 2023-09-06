60a73d20939c8f7806a7fa69cbb1ca991cc12bc6
abidhasanpintu <abidhasanpintu@gmail.com>
2023-09-06 21:01:44 +0600

    Update module.js

--- module.js
+++ module.js
@@ -1,9 +1,9 @@
 import topics from "./data/topics.json" assert {type: 'json'};
-import terms from "./data/terms.json" assert {type: 'json'};
-import category from "./data/category.json" assert {type: 'json'};
-import data from "./data/data.json" assert {type: 'json'};
+//import terms from "./data/terms.json" assert {type: 'json'};
+//import category from "./data/category.json" assert {type: 'json'};
+//import data from "./data/data.json" assert {type: 'json'};
 
 window.topicsData = topics;
-window.termsData = terms;
-window.category = category;
-window.mainData = data;
+//window.termsData = terms;
+//window.category = category;
+//window.mainData = data;
