bdca2c47c792268a6cf7522338f2b811dd09a1c6
abidhasanpintu <abidhasanpintu@gmail.com>
2023-09-06 20:51:55 +0600

    Create module.js

---
+++ module.js
@@ -0,0 +1,13 @@
+import topics from "./data/topics.json" assert {type: 'json'};
+import terms from "./data/terms.json" assert {type: 'json'};
+import category from "./data/category.json" assert {type: 'json'};
+import data from "./data/data.json" assert {type: 'json'};
+
+export default {
+	init() {
+		window.topicsData = topics;
+		window.termsData = terms;
+		window.category = category;
+		window.mainData = data;
+	}
+};
